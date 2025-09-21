bce5ce42719270f391b6fc2590e8eb34f09aabcc
u4916007249-dev <u4916007249@gmail.com>
2025-09-22 00:02:37 +0200

    Updated navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,12 +28,12 @@ export function Navbar() {
     { href: "/equipes", label: "Teams & Profile" },
     ...(isAuthenticated
       ? [
-          { href: "/recrutement", label: "Recrutement" },
           ...(user?.role === "admin" || user?.role === "developer"
             ? [{ href: "/admin", label: "Administration" }]
             : []),
         ]
       : []),
+              { href: "/recrutement", label: "Recrutement" },
     { href: "/profil", label: "Profil" },
     { href: "/credits", label: "Crédits" },
   ]
